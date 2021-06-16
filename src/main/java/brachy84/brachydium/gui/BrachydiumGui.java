e16c3a85af9a3369d1c05e7778587c00c284087a
brachy84 <brachy84@gmail.com>
2021-06-16 12:45:12 +0200

    mod setup

---
+++ src/main/java/brachy84/brachydium/gui/BrachydiumGui.java
@@ -0,0 +1,11 @@
+package brachy84.brachydium.gui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BrachydiumGui implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
