3fddb546056c9f31b124848906253a5a1c36dd3e
brachy84 <brachy84@gmail.com>
2021-06-30 12:06:08 +0200

    covert size to record

--- src/main/java/brachy84/brachydium/gui/api/math/Size.java
+++ src/main/java/brachy84/brachydium/gui/api/math/Size.java
@@ -1,26 +1,10 @@
 package brachy84.brachydium.gui.api.math;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 import java.util.Objects;
 
-public class Size implements JsonReader {
+public record Size(float width, float height) {
 
     public static final Size ZERO = new Size(0, 0);
-
-    public float width, height;
-
-    public Size(float width, float height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    public void resize(float w, float h) {
-        this.width = w;
-        this.height = h;
-    }
 
     public boolean isLargerThan(Size size) {
         return ((width - size.width) + (height - size.height)) > 0;
@@ -53,26 +37,5 @@ public class Size implements JsonReader {
                 "width=" + width +
                 ", height=" + height +
                 '}';
-    }
-
-    @Override
-    public Size readJson(JsonElement json) {
-        float width = 0, height = 0;
-        if(json.isJsonObject()) {
-            JsonObject j = json.getAsJsonObject();
-            if(j.has("width")) width = j.get("width").getAsFloat();
-            if(j.has("height")) height = j.get("height").getAsFloat();
-        } else if(json.isJsonArray()) {
-            JsonArray j = json.getAsJsonArray();
-            if(j.size() <= 1) {
-                width = j.get(0).getAsFloat();
-            }
-            if(j.size() <= 2) {
-                height = j.get(1).getAsFloat();
-            }
-        }
-        this.width = width;
-        this.height = height;
-        return new Size(width, height);
     }
 }
