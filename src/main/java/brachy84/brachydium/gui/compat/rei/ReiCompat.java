492e4fa8b69efc21f4e9c0d0f57953bffc7b417a
brachy84 <brachy84@gmail.com>
2021-11-15 21:50:15 +0100

    fix rei recipe view interaction

--- src/main/java/brachy84/brachydium/gui/compat/rei/ReiCompat.java
+++ src/main/java/brachy84/brachydium/gui/compat/rei/ReiCompat.java
@@ -1,15 +1,19 @@
 package brachy84.brachydium.gui.compat.rei;
 
+import brachy84.brachydium.gui.api.Interactable;
 import brachy84.brachydium.gui.api.math.AABB;
 import brachy84.brachydium.gui.api.math.Pos2d;
+import brachy84.brachydium.gui.api.widgets.ItemSlotWidget;
 import brachy84.brachydium.gui.internal.Gui;
 import brachy84.brachydium.gui.internal.GuiScreen;
 import brachy84.brachydium.gui.internal.UiHandler;
+import dev.architectury.event.CompoundEventResult;
 import me.shedaniel.math.Rectangle;
 import me.shedaniel.rei.api.client.gui.config.DisplayPanelLocation;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.screen.DisplayBoundsProvider;
 import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.util.ActionResult;
 
@@ -18,6 +22,15 @@ public class ReiCompat implements REIClientPlugin {
     @Override
     public void registerScreens(ScreenRegistry registry) {
         registry.registerDecider(new ReiGuiScreen());
+        registry.registerFocusedStack((screen, mouse) -> {
+            if (screen instanceof GuiScreen guiScreen) {
+                Interactable interactable = guiScreen.getHoveredInteractable(Pos2d.ofReiPoint(mouse));
+                if (interactable instanceof ItemSlotWidget slotWidget) {
+                    return CompoundEventResult.interruptTrue(EntryStacks.of(slotWidget.getResource()));
+                }
+            }
+            return CompoundEventResult.pass();
+        });
     }
 
     public static class ReiGuiScreen implements DisplayBoundsProvider<GuiScreen> {
